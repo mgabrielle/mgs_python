54539c9c7f8246bd57ee7bfe98f854a962e3750b
mgabrielle <mgabriellesoouza@gmail.com>
2023-09-12 01:29:33 +0000

    corrrigindo falhas e inserindo dados na base

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template, request, session, abort, flash, redirect, url_for
-
+from flask import Flask, render_template, g, request, session, abort, flash, redirect, url_for
+import sqlite3
 
 app = Flask('Meu app')
 app.config['SECRET_KEY'] = 'pudim'
 
-posts = [
-    {
-        'titulo': 'Minha primeira postagem',
-        'texto': 'teste'
-    },
-    {
-        'titulo': 'Segundo Post',
-        'texto': 'teste'
-    }
-]
+app.config.from_object(__name__)
+DATABASE = 'banco.db'
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.db = conectar()
+
+@app.teardown_request
+def teardown_request(f):
+    g.db.close() 
+
 
 @app.route('/')
 def exibir_entradas():
-    entradas = posts[::-1] # Mock das postagens
-    return render_template('exibir_entradas.html', entradas=entradas)
+    #entradas = posts[::-1] # Mock das postagens
+    sql = 'SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC'
+    resultado = g.db.execute(sql)
+    entrada = []
+
+    for titulo, texto, data_criacao in resultado.fetchall():
+        entrada.append(
+            {
+                'titulo': titulo,
+                'texto': texto,
+                'data_criacao': data_criacao
+            }
+        )
+
+    return render_template('exibir_entradas.html', entradas=entrada)
 
 #rota login
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,23 +56,28 @@ def logout():
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('exibir_entradas'))
 
-# nova rota para POSTS
-@app.route('/inserir', methods=['POST'])
+
+#nova rota para POSTS
+@app.route('/inserir', methods=["POST"])
 def inserir_entradas():
-    if session['logado']:
-        novo_post = {
-            'titulo': request.form['titulo'],
-            'texto': request.form['texto']
-            }
-        posts.append(novo_post)
-        flash('Post criado com sucesso')
+    if not session['logado']:
+        abort(401)
+        
+    titulo = request.form.get('titulo')  
+    texto = request.form.get('texto')  
+    sql = "INSERT INTO posts (titulo, texto) values(?,?) "
+    g.db.execute(sql,[titulo, texto])
+    g.db.commit()
+    flash("Post criado com sucesso!")
     return redirect(url_for('exibir_entradas'))
 
-# mostrar apenas uma entrada
-@app.route('/posts/<int:id>')
-def exibir_entrada(id):
-    try:
-        entrada = posts[id-1]
-        return render_template('exibir_entrada.html', entrada=entrada)
-    except Exception:
-        return abort(404)
+
+
+#mostrar apenas uma entrada
+# @app.route('/posts/<int:id>')
+# def exibir_entrada(id):
+#     try:
+#         entrada = posts[id-1]
+#         return render_template('exibir_entrada.html', entrada=entrada)
+#     except Exception:
+#         return abort(404)
